d2a407129321ac83d361a7fd71567b7bf3c7d78d
CornerOfSkyline <zhangjianfei2005@126.com>
2022-06-22 16:02:04 +0800

    xap_aruco中去掉gui

--- aruco/xap_aruco/xap_aruco.cpp
+++ aruco/xap_aruco/xap_aruco.cpp
@@ -24,6 +24,7 @@ Copyright 2020 Rafael Muñoz Salinas. All rights reserved.
 #include <string>
 #include <stdexcept>
 #include <xap_external_protocol.h>
+#include <xap_external_protocol_decode.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -136,62 +137,6 @@ void setParamsFromGlobalVariables(aruco::MarkerDetector &md) {
         md.setDictionary((aruco::Dictionary::DICT_TYPES) iDictionaryIndex,float(iCorrectionRate)/10. );  // sets the dictionary to be employed (ARUCO,APRILTAGS,ARTOOLKIT,etc)
 }
 
-void createMenu() {
-    cv::createTrackbar("Dictionary", "menu", &iDictionaryIndex, 13, cvTackBarEvents);
-    cv::createTrackbar("DetectMode", "menu", &iDetectMode, 2, cvTackBarEvents);
-    cv::createTrackbar("CornerMode", "menu", &iCornerMode, 2, cvTackBarEvents);
-    cv::createTrackbar("Track", "menu", &iTrack,1, cvTackBarEvents);
-
-    cv::createTrackbar("MinMarkerSize", "menu", &iMinMarkerSize, 1000, cvTackBarEvents);
-    cv::createTrackbar("Threshold", "menu", &iThreshold, 40, cvTackBarEvents);
-    cv::createTrackbar("ErrorRate", "menu", &iCorrectionRate, 10, cvTackBarEvents);
-    cv::createTrackbar("Enclosed", "menu", &iEnclosed, 1, cvTackBarEvents);
-    cv::createTrackbar("ShowAll", "menu", &iShowAllCandidates, 1, cvTackBarEvents);
-    iThreshold=MDetector.getParameters().ThresHold;
-    iCornerMode= MDetector.getParameters().cornerRefinementM;
-}
-
-void putText(cv::Mat &im,string text,cv::Point p,float size) {
-    float fact=float(im.cols)/float(640);
-    if (fact<1) fact=1;
-
-    cv::putText(im,text,p,FONT_HERSHEY_SIMPLEX, size,cv::Scalar(0,0,0),3*fact);
-    cv::putText(im,text,p,FONT_HERSHEY_SIMPLEX, size,cv::Scalar(125,255,255),1*fact);
-
-}
-void printHelp(cv::Mat &im)
-{
-    float fs=float(im.cols)/float(1200);
-
-    putText(im,"'m': show/hide menu",cv::Point(10,fs*60),fs*0.5f);
-    putText(im,"'s': start/stop video capture",cv::Point(10,fs*80),fs*0.5f);
-    putText(im,"'w': write image to file",cv::Point(10,fs*100),fs*0.5f);
-    putText(im,"'t': do a speed test",cv::Point(10,fs*120),fs*0.5f);
-    putText(im,"'f': saves current configuration to file 'arucoConfig.yml'",cv::Point(10,fs*140),fs*0.5f);
-}
-
-void printInfo(cv::Mat &im) {
-    float fs=float(im.cols)/float(1200);
-    putText(im,"fps="+to_string(1./Fps.getAvrg()),cv::Point(10,fs*20),fs*0.5f);
-    putText(im,"'h': show/hide help",cv::Point(10,fs*40),fs*0.5f);
-    if(bPrintHelp) printHelp(im);
-}
-
-void printMenuInfo() {
-    cv::Mat image(200,400,CV_8UC3);
-    image=cv::Scalar::all(255);
-    string str="Dictionary="+aruco::Dictionary::getTypeString((aruco::Dictionary::DICT_TYPES) iDictionaryIndex) ;
-
-    cv::putText(image,str,cv::Size(10,20),FONT_HERSHEY_SIMPLEX, 0.35,cv::Scalar(0,0,0),1);
-
-    str="Detection Mode="+MarkerDetector::Params::toString(MDetector.getParameters().detectMode);
-    cv::putText(image,str,cv::Size(10,40),FONT_HERSHEY_SIMPLEX, 0.35,cv::Scalar(0,0,0),1);
-    str="Corner Mode="+MarkerDetector::Params::toString(MDetector.getParameters().cornerRefinementM);;
-    cv::putText(image,str,cv::Size(10,60),FONT_HERSHEY_SIMPLEX, 0.35,cv::Scalar(0,0,0),1);
-    cv::imshow("menu",image);
-}
-
-
 /************************************
  *
  *
@@ -244,7 +189,26 @@ bool sendMessage(int serial_fd, const uint8_t msgClass, const uint8_t msgID, con
     return true;
 }
 
+typedef struct {
+    uint32_t org_timestamp;
+    uint32_t rec_timestamp;
+    uint32_t xmt_timestamp;
 
+    uint32_t event_count;
+
+    float theta_least;
+    float theta_most;
+    float theta_mean;
+    float theta_M2;
+    float theta_rms;
+
+    float delta_least;
+    float delta_most;
+    float delta_mean;
+    float delta_M2;
+    float delta_rms;
+
+} xapTimeSyncState_t;
 
 int main(int argc, char** argv)
 {
@@ -281,7 +245,7 @@ int main(int argc, char** argv)
             serial_fd = open(serial_name, O_RDWR | O_NOCTTY | O_NONBLOCK);
 
             if (serial_fd < 0) {
-                cout << "pen failed （" << errno << ")" << endl;
+                cout << "open failed （" << errno << ")" << endl;
                 return -1;
             }
 
@@ -297,6 +261,8 @@ int main(int argc, char** argv)
 
             /* no parity, one stop bit */
             uart_config.c_cflag &= ~(CSTOPB | PARENB);
+
+            uart_config.c_lflag  &= ~(ICANON | ECHO | ECHOE | ISIG);
 
             unsigned speed = B921600;
 
@@ -316,6 +282,11 @@ int main(int argc, char** argv)
                 return -1;
             }
         }
+
+        /* 串口通讯 */
+        xap_external_protocol_decoder_t xap_external_protocol_decoder;  // 协议解析器
+        uint8_t buff_in[512];       // 串口接收数据缓冲区
+        uint8_t buff_out[512];      // 串口发送数据缓冲区
 
         float TheMarkerSize = std::stof(cml("-s", "-1"));
         //resize factor
@@ -365,30 +336,6 @@ int main(int argc, char** argv)
         if (!TheVideoCapturer.isOpened())
             throw std::runtime_error("Could not open video");
 
-
-        //create windows
-        if(cml["-fs"]) {
-            cv::namedWindow("in", cv::WINDOW_FULLSCREEN);
-            cv::namedWindow("thres", cv::WINDOW_FULLSCREEN);
-        }
-        else if(cml["-ws"])
-        {
-            cv::namedWindow("in",cv::WINDOW_NORMAL);
-            cv::Size s=parseSize(cml("-ws"));
-            cv::resizeWindow("in",s.width,s.height);
-            cv::namedWindow("thres",cv::WINDOW_NORMAL);
-            resizeWindow("thres",s.width,s.height);
-
-        }
-
-        else {
-            cv::namedWindow("in",cv::WINDOW_NORMAL);
-            cv::resizeWindow("in",640,480);
-            float w=std::min(int(1920),int(TheInputImage.cols));
-            float f=w/float(TheInputImage.cols);
-            resizeWindow("in",w,float(TheInputImage.rows)*f);
-        }
-
         ///// CONFIGURE DATA
         // read first image to get the dimensions
         TheVideoCapturer >> TheInputImage;
@@ -405,8 +352,12 @@ int main(int argc, char** argv)
 
 
         // go!
-        char key = 0;
-        int index = 0,indexSave=0;
+        struct timespec sync_timestamp = {0,0};
+
+        xapTimeSyncState_t xapTimeSyncState = {0};
+
+        /* 串口协议解析器初始化 */
+        xapExternalProtocolDecodeInit(&xap_external_protocol_decoder);
         // capture until press ESC or until the end of the video
 
         do
@@ -421,15 +372,6 @@ int main(int argc, char** argv)
             Fps.stop();
             // chekc the speed by calculating the mean speed of all iterations
             cout << "\rTime detection=" << Fps.getAvrg()*1000 << " milliseconds nmarkers=" << TheMarkers.size() <<" images resolution="<<TheInputImage.size() <<std::endl;
-
-            // print marker info and draw the markers in image
-            TheInputImage.copyTo(TheInputImageCopy);
-
-            if (iShowAllCandidates) {
-                auto candidates=MDetector.getCandidates();
-                for(auto cand:candidates)
-                    Marker(cand,-1).draw(TheInputImageCopy, Scalar(255, 0, 255));
-            }
 
             for (unsigned int i = 0; i < TheMarkers.size(); i++)
             {
@@ -461,114 +403,89 @@ int main(int argc, char** argv)
                 }
             }
 
-            // draw a 3d cube in each marker if there is 3d info
-            if (TheCameraParameters.isValid() && TheMarkerSize > 0)
-                for (unsigned int i = 0; i < TheMarkers.size(); i++)
+            int dataNum = read(serial_fd, buff_in, sizeof(buff_in));
+            int cmd_id = -1;
+            for (int i = 0; i < dataNum; ++i)
+            {
+                // cout << "Serial Port Received" << dataNum << " Bytes! From port fd " << serial_fd <<endl;
+                cmd_id = xapExternalProtocolParseChar(&xap_external_protocol_decoder, buff_in[i]);
+                switch (cmd_id >> 8)
                 {
-                    if(TheMarkers[i].id == 7) {
-                        CvDrawingUtils::draw3dCube(TheInputImageCopy, TheMarkers[i], TheCameraParameters);
-                        CvDrawingUtils::draw3dAxis(TheInputImageCopy, TheMarkers[i], TheCameraParameters);
+                case XAP_EXTERNAL_CLASS_COMMON_TIME:
+
+                    switch (cmd_id & 0x00FF)
+                    {
+                    case XAP_EXTERNAL_COMMON_TIME_SYNC:
+                        // cout << "Received one time sync packet!" << endl;
+                        clock_gettime(CLOCK_MONOTONIC, &sync_timestamp);
+
+                        xapTimeSyncState.rec_timestamp = sync_timestamp.tv_sec * 1000 + sync_timestamp.tv_sec / 1e6;
+                        xapTimeSyncState.org_timestamp =
+                            xap_external_protocol_decoder.rx_buffer.xap_external_common_time_sync.xmt_timestamp;
+
+                        /* 设置时间同步帧消息 */
+                        xap_external_common_time_sync_t xap_external_common_time_sync;
+
+                        xap_external_common_time_sync.org_timestamp = xapTimeSyncState.org_timestamp;
+                        xap_external_common_time_sync.rec_timestamp = xapTimeSyncState.rec_timestamp;
+                        xap_external_common_time_sync.xmt_timestamp = sync_timestamp.tv_sec * 1000 + sync_timestamp.tv_sec / 1e6;
+
+                        /* 记录时间同步帧发送时刻 */
+                        xapTimeSyncState.xmt_timestamp = xap_external_common_time_sync.xmt_timestamp;
+
+                        sendMessage(serial_fd, XAP_EXTERNAL_CLASS_COMMON_TIME, XAP_EXTERNAL_COMMON_TIME_SYNC, (const uint8_t*)&xap_external_common_time_sync, sizeof(xap_external_common_time_sync));
+
+                        if (xapTimeSyncState.xmt_timestamp ==
+                                xap_external_protocol_decoder.rx_buffer.xap_external_common_time_sync.org_timestamp) {
+                            if (xap_external_protocol_decoder.rx_buffer.xap_external_common_time_sync.rec_timestamp != 0
+                                    & xap_external_protocol_decoder.rx_buffer.xap_external_common_time_sync.xmt_timestamp != 0) {
+                            }
+
+                            int32_t T1 = xapTimeSyncState.xmt_timestamp;
+                            int32_t T2 = xap_external_protocol_decoder.rx_buffer.xap_external_common_time_sync.rec_timestamp;
+                            int32_t T3 = xapTimeSyncState.org_timestamp;
+                            int32_t T4 = xapTimeSyncState.rec_timestamp;
+
+                            float theta = ((T2 - T1) + (T3 - T4)) / 2 / 1e3f;
+                            float delta = ((T4 - T1) - (T3 - T2)) / 2 / 1e3f;
+
+                            xapTimeSyncState.event_count ++;
+
+                            float d_theta = theta - xapTimeSyncState.theta_mean;
+                            xapTimeSyncState.theta_mean += d_theta / xapTimeSyncState.event_count;
+                            xapTimeSyncState.theta_M2 += d_theta * (theta - xapTimeSyncState.theta_mean);
+                            xapTimeSyncState.theta_least = min(d_theta, xapTimeSyncState.theta_least);
+                            xapTimeSyncState.theta_most = max(d_theta, xapTimeSyncState.theta_most);
+                            xapTimeSyncState.theta_rms = sqrtf(xapTimeSyncState.theta_M2 / (xapTimeSyncState.event_count -
+                                                               1));
+
+                            float d_delta = delta - xapTimeSyncState.delta_mean;
+                            xapTimeSyncState.delta_mean += d_delta / xapTimeSyncState.event_count;
+                            xapTimeSyncState.delta_M2 += d_delta * (delta - xapTimeSyncState.delta_mean);
+                            xapTimeSyncState.delta_least = min(d_delta, xapTimeSyncState.delta_least);
+                            xapTimeSyncState.delta_most = max(d_delta, xapTimeSyncState.delta_most);
+                            xapTimeSyncState.delta_rms = sqrtf(xapTimeSyncState.delta_M2 / (xapTimeSyncState.event_count -
+                                                               1));
+                        }
+                        break;
+
+                    default:
+                        break;
                     }
+                    break;
+
+                default:
+                    break;
                 }
-
-            // DONE! Easy, right?
-            // show input with augmented information and  the thresholded image
-            printInfo(TheInputImageCopy);
-            if(showMennu)printMenuInfo();
-
-            cv::imshow("thres", resize(MDetector.getThresholdedImage(), 1024));
-
-            cv::imshow("in",  TheInputImageCopy);
-
-            key = cv::waitKey(waitTime);  // wait for key to be pressed
-            if (key == 's')
-                waitTime = waitTime == 0 ? 10 : 0;
-            if (key == 'w') { //writes current input image
-                string number=std::to_string(indexSave++);
-                while(number.size()!=3)number="0"+number;
-                string imname="arucoimage"+number+".png";
-                cv::imwrite(imname,TheInputImageCopy);
-                cout<<"saved "<<imname<<endl;
-                imname="orgimage"+number+".png";
-                cv::imwrite(imname,TheInputImage);
-                cout<<"saved "<<imname<<endl;
-                imname="thresimage"+number+".png";
-                cv::imwrite(imname,MDetector.getThresholdedImage());
-
             }
-            if (key=='m') {
-                if (showMennu)                     cv::destroyWindow("menu");
-                else {
-                    cv::namedWindow("menu",cv::WINDOW_NORMAL);
-                    cv::resizeWindow("menu",640,480);
-                    createMenu();
-                    printMenuInfo();
-                }
-                showMennu=!showMennu;
-            }
-            if (key=='h')bPrintHelp=!bPrintHelp;
-
-            if (key=='t') { //run a deeper speed test
-
-                for(int t=0; t<30; t++) {
-                    // Detection of markers in the image passed
-                    Fps.start();
-                    TheMarkers = MDetector.detect(TheInputImage, TheCameraParameters, TheMarkerSize);
-                    Fps.stop();
-                    // chekc the speed by calculating the mean speed of all iterations
-                }
-                printInfo(TheInputImageCopy);
-            }
-            if(key=='f') {
-                cerr<<"Configuration saved to arucoConfig.yml"<<endl;
-                MDetector.saveParamsToFile("arucoConfig.yml");
-            }
-            index++;  // number of images captured
 
             if (isVideo)
-                if ( TheVideoCapturer.grab()==false) key=27;
-        } while (key != 27 );
+                if ( TheVideoCapturer.grab()==false) break;
+        } while (true);
     }
     catch (std::exception& ex)
 
     {
         cout << "Exception :" << ex.what() << endl;
     }
-}
-
-
-void cvTackBarEvents(int pos, void*)
-{
-    (void)(pos);
-
-
-    setParamsFromGlobalVariables(MDetector);
-
-    // recompute
-    Fps.start();
-    TheMarkers=MDetector.detect(TheInputImage);
-    Fps.stop();
-    // chekc the speed by calculating the mean speed of all iterations
-    TheInputImage.copyTo(TheInputImageCopy);
-    if (iShowAllCandidates) {
-        auto candidates=MDetector.getCandidates();
-        for(auto cand:candidates)
-            Marker(cand,-1).draw(TheInputImageCopy, Scalar(255, 0, 255),1);
-    }
-
-    for (unsigned int i = 0; i < TheMarkers.size(); i++) {
-        cout << TheMarkers[i] << endl;
-        TheMarkers[i].draw(TheInputImageCopy, Scalar(0, 0, 255),2);
-    }
-
-    // draw a 3d cube in each marker if there is 3d info
-    if (TheCameraParameters.isValid())
-        for (unsigned int i = 0; i < TheMarkers.size(); i++)
-            CvDrawingUtils::draw3dCube(TheInputImageCopy, TheMarkers[i], TheCameraParameters);
-    cv::putText(TheInputImageCopy,"fps="+to_string(1./Fps.getAvrg() ),cv::Point(10,20),FONT_HERSHEY_SIMPLEX, 0.5f,cv::Scalar(125,255,255),2);
-
-    cv::imshow("in",  TheInputImageCopy );
-    cv::imshow("thres", resize(MDetector.getThresholdedImage(), 1024));
-    if(showMennu)printMenuInfo();
-
 }
